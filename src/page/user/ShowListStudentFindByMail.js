d3dd30a774f1800ea4b47d14f4a8791b8433679b
Nguyenhadz <nguyenha210895@gmail.com>
2023-12-11 15:20:06 +0700

    update giao diện

--- src/page/user/ShowListStudentFindByMail.js
+++ src/page/user/ShowListStudentFindByMail.js
@@ -112,8 +112,8 @@ export default function ShowListStudentFindByMail() {
                                 onClick={handleSearch}>Search
                         </button>
                     </div>
-                    <div className={"flex items-center justify-center mt-5 mb-5"}><h1 className={"text-5xl"}>Danh sách
-                        học sinh</h1></div>
+                    <div className={"text-5xl font-extrabold font-sans text-orange-500 mt-5 mb-5 flex justify-center"}>Danh sách học sinh
+                    </div>
 
                     <Box sx={{
                         height: '630px',
@@ -159,8 +159,9 @@ export default function ShowListStudentFindByMail() {
                                 onClick={handleSearch}>Search
                         </button>
                     </div>
-                    <div className={"flex items-center justify-center mt-5 mb-5"}><h1 className={"text-5xl text-orange-600"}><b>Không có
-                        học sinh nào có email bạn muốn tìm</b></h1></div>
+                    <div className={"text-5xl font-extrabold font-sans text-orange-500 mt-5 mb-5 flex justify-center"}>Không có
+                        học sinh nào có email bạn muốn tìm
+                    </div>
 
                 </div>
             )}
