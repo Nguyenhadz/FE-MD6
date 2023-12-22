22135b62bf34b867e43aa640950c9005ea368744
Kien Nguyen <kien05071995@gmail.com>
2023-12-23 00:22:39 +0700

    update ui

--- src/component/ListQuizCard.js
+++ src/component/ListQuizCard.js
@@ -13,97 +13,42 @@ export default function ListQuizCard() {
         return state.quizzes.quizzes;
     })
     useEffect(() => {
-            dispatch(findAllQuiz());
-        }, [])
+        dispatch(findAllQuiz());
+    }, [])
+    const categories = [...new Set(quizzes.map((quiz) => quiz.category))];
+    const filterByCategory = (quizzes, category) => {
+        return quizzes.filter((quiz) => quiz.category === category);
+    };
     return (
         <ImageList
             sx={{
                 gridAutoFlow: "column",
                 gridTemplateColumns: "repeat(auto-fit, minmax(160px,1fr)) !important",
                 gridAutoColumns: "minmax(160px, 1fr)",
-                scrollbarWidth:100
+                scrollbarWidth: 100
             }}
         >
-            {quizzes.map((quiz) => (
-                <ImageListItem
-                    key={quiz.id}
-                >
-                    <img
-                        srcSet={quiz.image}
-                        src={quiz.image}
-                        alt={quiz.title}
-                        loading="lazy"
-                    />
-                    <ImageListItemBar
-                        title={quiz.title}
-                        subtitle={<span>by: {quiz.description}</span>}
-                        position="below"
-                    />
+            {categories.map((category) => (
+                <ImageListItem key={category}>
+                    <h3>{category}</h3>
+                    {filterByCategory(quizzes, category).map((quiz) => (
+                        <div>
+                            <img
+                                srcSet={quiz.image}
+                                src={quiz.image}
+                                alt={quiz.title}
+                                loading="lazy"
+                            />
+                            <ImageListItemBar
+                                title={quiz.title}
+                                subtitle={<span>by: {quiz.description}</span>}
+                                position="below"
+                            />
+                        </div>
+                    ))}
                 </ImageListItem>
             ))}
+
         </ImageList>
     );
 }
-
-const itemData = [
-    {
-        img: '',
-        title: 'Breakfast',
-        author: '@bkristastucchio',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-        title: 'Burger',
-        author: '@rollelflex_graphy726',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-        title: 'Camera',
-        author: '@helloimnik',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-        title: 'Coffee',
-        author: '@nolanissac',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-        title: 'Hats',
-        author: '@hjrc33',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-        title: 'Honey',
-        author: '@arwinneil',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-        title: 'Basketball',
-        author: '@tjdragotta',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-        title: 'Fern',
-        author: '@katie_wasserman',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-        title: 'Mushrooms',
-        author: '@silverdalex',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-        title: 'Tomato basil',
-        author: '@shelleypauls',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-        title: 'Sea star',
-        author: '@peterlaster',
-    },
-    {
-        img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-        title: 'Bike',
-        author: '@southside_customs',
-    },
-];
