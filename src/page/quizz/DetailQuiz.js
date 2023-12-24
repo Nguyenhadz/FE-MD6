5d4be12ed8497710b77a5181b09aaa69b0ad0720
Hun191189 <tranmanhhung191189@gmai.com>
2023-12-24 23:24:18 +0700

    sua lich sử thi

--- src/page/quizz/DetailQuiz.js
+++ src/page/quizz/DetailQuiz.js
@@ -83,84 +83,102 @@ const DetailQuiz = ({quizId}) => {
 
     return (
         <div className={"ml-8"}>
-            {/*<Modal*/}
-            {/*    open={open}*/}
-            {/*    onClose={handleClose}*/}
-            {/*    aria-labelledby="modal-modal-title"*/}
-            {/*    aria-describedby="modal-modal-description"*/}
-            {/*>*/}
-            {/*    <Box id="modal-container" sx={{*/}
-            {/*        width: '1200px',*/}
-            {/*        height: '800px',*/}
-            {/*        position: 'absolute',*/}
-            {/*        top: '0',*/}
-            {/*        left: '20%',*/}
-            {/*        backgroundColor: 'background.paper',*/}
-            {/*        display: 'flex',*/}
-            {/*        flexDirection: 'column'*/}
-            {/*    }}>*/}
-            {/*        <Typography id="modal-modal-title" variant="h6" component="h2">*/}
-            {/*            <EditQuiz quiz={quiz}/>*/}
-            {/*        </Typography>*/}
-            {/*    </Box>*/}
-            {/*</Modal>*/}
-            <div className="flex items-center justify-between mb-3">
+            <div className={'flex justify-between text-xl'}>
                 <div>
-                    <b>Tên bài thi: &nbsp;
-                        <span dangerouslySetInnerHTML={{__html: quiz.title}}></span>
-                    </b>
+                    <div className="flex items-center justify-between mb-3">
+                        <div>
+                            <b>Tên bài thi: &nbsp;
+                                <span dangerouslySetInnerHTML={{__html: quiz.title}}></span>
+                            </b>
+                        </div>
+
+                    </div>
+                    <div className={"mb-3"}><span className={'font-bold'}> Mô tả: &nbsp;</span> <span
+                        dangerouslySetInnerHTML={{__html: quiz.description}}></span>
+                    </div>
+                    <div className={"mb-3"}><span className={'font-bold'}> Người tạo: &nbsp;</span> {quiz.user?.name}
+                    </div>
+                    <div className={"mb-3"}><span
+                        className={'font-bold'}> Số lượng câu hỏi: &nbsp;</span> {quiz.questions?.length}</div>
+                    <div className={"flex mb-3"}>Thể loại: &nbsp; <span
+                        dangerouslySetInnerHTML={{__html: quiz.categoryQuiz?.name}}></span></div>
+                    <div className={"mb-3"}><span className={'font-bold'}> Mức độ: &nbsp;</span> {quiz.levelQuiz?.name}
+                    </div>
+                    <div className={"mb-3"}><span className={'font-bold'}> Điểm đạt: &nbsp;</span> {quiz.passScore}
+                    </div>
+                    <div className={"mb-3"}><span
+                        className={'font-bold'}> Thời gian thi: &nbsp;</span> {minute} &nbsp;phút, {second} &nbsp;giây
+                    </div>
                 </div>
-                <div className="flex items-center">
-                    <div className="ml-auto">
+
+                <div className={'mr-10'}>
+                    <img src={quiz.image} alt={"fault"} style={{width: '400px', height: '300px'}}/>
+                    <div className={"flex mt-3 border-solid border-2 rounded border-orange-200"}><span
+                        className={'font-bold ml-3'}> Thể loại: &nbsp;</span> <span
+                        dangerouslySetInnerHTML={{__html: quiz.categoryQuiz?.name}}></span></div>
+                </div>
+            </div>
+
+            <div className={'mt-10'}>
+                {quiz.questions?.map((question, index) => (
+                    <Accordion key={index}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon/>}
+                            aria-controls={`panel${index + 1}-content`}
+                            id={`panel${index + 1}-header`}
+                        >
+                            <Typography
+                                sx={{
+                                    fontSize: '18px',
+                                    fontWeight: 'medium',
+                                }}
+                            >Câu hỏi {index + 1}: &nbsp;<span
+                                dangerouslySetInnerHTML={{__html: question.content}}></span></Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>
+                                <Box sx={{width: "100%"}}>
+                                    <Grid container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
+                                        {question.answers.map((answer, answerIndex) => (
+                                            <Grid item xs={6} key={answerIndex}>
+                                                <Item
+                                                    sx={{
+                                                        height:'100%',
+                                                        backgroundColor: answer.status === 1 ? '#99FFFF' : 'white',
+                                                        fontWeight: answer.status === 1 ? 'bold' : '',
+                                                        textAlign: 'left',
+                                                        fontSize: '18px'
+                                                    }}
+                                                ><span dangerouslySetInnerHTML={{__html: answer.content}}
+                                                            style={{color: answer.status === 1 ? 'blue' : 'black'}}></span></Item>
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </Box>
+                            </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                ))}
+                <div className="flex items-center justify-center">
+                    <div className="m-3">
                         <button type="submit"
-                                className={"h-6 w-32 bg-gray-50 mt-2 border-2 rounded-full hover:hover:bg-blue-50"} onClick={()=> {handleShowAllResultByQuiz(quizId)}}>
-                            Lịch sử
+                                className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10  mx-auto"}
+                                onClick={() => {
+                                    handleShowAllResultByQuiz(quizId)
+                                }}>
+                            Lịch Sử Thi
                         </button>
                     </div>
-                    <div className="ml-4">
-                        <button type="submit" onClick={() => {handleEdit(quizId)}}
-                                className={"h-6 w-32 bg-gray-50 mt-2 border-2 rounded-full hover:hover:bg-blue-50"}>
-                            Sửa bài thi
+                    <div className="">
+                        <button type="submit" onClick={() => {
+                            handleEdit(quizId)
+                        }}
+                                className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10  mx-auto"}>
+                            Sửa Bài Thi
                         </button>
                     </div>
                 </div>
             </div>
-            <div className={"mb-3"}>Mô tả: &nbsp;<span dangerouslySetInnerHTML={{__html: quiz.description}}></span>
-            </div>
-            <div className={"mb-3"}>Người tạo: &nbsp; {quiz.user?.name}</div>
-            <div className={"mb-3"}>Số lượng câu hỏi: &nbsp; {quiz.questions?.length}</div>
-            <div className={"flex mb-3"}>Thể loại: &nbsp; <span
-                dangerouslySetInnerHTML={{__html: quiz.categoryQuiz?.name}}></span></div>
-            <div className={"mb-3"}>Mức độ: &nbsp; {quiz.levelQuiz?.name}</div>
-            <div className={"mb-3"}>Điểm đạt: &nbsp; {quiz.passScore}</div>
-            <div className={"mb-3"}>Thời gian thi: &nbsp; {minute} &nbsp;phút, {second} &nbsp;giây</div>
-            <div className={"mb-3"}>Câu hỏi:</div>
-            {quiz.questions?.map((question, index) => (
-                <Accordion key={index}>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon/>}
-                        aria-controls={`panel${index + 1}-content`}
-                        id={`panel${index + 1}-header`}
-                    >
-                        <Typography>Câu hỏi {index + 1}: &nbsp;<span
-                            dangerouslySetInnerHTML={{__html: question.content}}></span></Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <Typography>
-                            <Box sx={{width: "100%"}}>
-                                <Grid container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
-                                    {question.answers.map((answer, answerIndex) => (
-                                        <Grid item xs={6} key={answerIndex}>
-                                            <Item><span dangerouslySetInnerHTML={{__html: answer.content}}
-                                                        style={{color: answer.status === 1 ? 'blue' : 'black'}}></span></Item>
-                                        </Grid>
-                                    ))}
-                                </Grid>
-                            </Box>
-                        </Typography>
-                    </AccordionDetails>
-                </Accordion>
-            ))}
 
         </div>
     );
